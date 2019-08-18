f5ae9cdf4dd50aad932b08796e787bb9882a3230
Clayton <herbsca1@gmail.com>
2019-08-18 18:33:25 +0800

    completed week 3 lab

--- week3/task1.c
+++ week3/task1.c
@@ -4,25 +4,33 @@
 #include <unistd.h>
 #include <time.h>
 
-#define SIZE_ARRAY 100000
+#define SIZE_ARRAY 10000
+//#define NUM_THREADS 1
 
 float arr[SIZE_ARRAY];
 
 void initLargeArray(void) {
     for(int i = 0; i < SIZE_ARRAY; i++) {
-        arr[i] = (float) rand();
+        arr[i] = (float) abs(rand());
     }
-    printf("%f", arr[255]);
+    printf("%f\n", arr[255]);
+    printf("%f\n", arr[1120]);
 }
 
 int main(int argc, char *argv[]) {
-    time_t start = time(NULL);
-    clock_t start_t, end_t;
+    if(argc != 2) {
+        fprintf(stderr, "Usage: ./t1 NUM_THREADS\n");
+        exit(EXIT_FAILURE);
+    }
+    clock_t start_t, end_t, total_t;
 
-    initLargeArray();
+    start_t = clock();
+    int num_threads = atoi(argv[1]);
     float sum = 0;
     float localsum = 0;
 
+    initLargeArray();
+    omp_set_num_threads(num_threads);
 
     #pragma omp parallel private(localsum)
     {
@@ -33,13 +41,17 @@ int main(int argc, char *argv[]) {
         {
                 localsum += arr[i];
         }
+        printf("localsum = %f\n", localsum);
         sum += localsum; //transfer local memory to sequential scope variable
     }
 
+    printf("-- > localsum = %f\n", localsum);
     printf("\nsum = %.1f\n\n", sum);
+    end_t = clock();
+    total_t = (end_t - start_t) / CLOCKS_PER_SEC;
+    printf("%d :: Time difference = %ld \n", omp_get_thread_num(), total_t);
 
-    time_t end = time(NULL);
-    printf("%d :: Time difference = %2.1f\n", omp_get_thread_num(), difftime(end, start));
+
 
     fprintf(stdout, "\nProgram finished running.\n");
     exit(EXIT_SUCCESS);
