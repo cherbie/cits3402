c7e110514bb9dc34c5da7ff2cb9eb5bdcfd2f9de
Clayton <herbsca1@gmail.com>
2019-09-24 11:37:41 +0800

    testing script

--- matrices/app/test.sh
+++ matrices/app/test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 clear
 echo "start"
-./mop --ad -f ./input/file3.in ./input/file4.in
-./mop --ad -f ./input/int1-3.in ./input/int2-3.in 
+./mop --ad -f ./input/float1024.in ./input/float1024.in
+./mop --ad -f ./input/int1024.in ./input/int1024.in
