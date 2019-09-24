7b08e6282b7f9e7513c7b900298589bd03b231a6
Clayton <herbsca1@gmail.com>
2019-09-24 22:53:10 +0800

    logging of coo updated

--- matrices/app/testi.sh
+++ matrices/app/testi.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 clear
-./mop --ad -f ./input/int1024.in ./input/int1024.in -s
-./mop --ad -f ./input/int1024.in ./input/int1024.in -t 4
+./mop --sc 2 -f ./input/int1024.in -s
+./mop --sc 2 -f ./input/int1024.in -t 4
 echo "... script complete"
