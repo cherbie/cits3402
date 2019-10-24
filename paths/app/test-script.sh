7df8429774618b2dab8274faffbfa412ef999bc2
Clayton Herbst <22245091@ecm.uwa.edu.au>
2019-10-25 00:18:52 +0800

    test-script updated

--- paths/app/test-script.sh
+++ paths/app/test-script.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 clear
-mpirun --hostfile hostfile ./sp -f ./examples/4.in
-echo "... completed 4"
-mpirun --hostfile hostfile ./sp -f ./examples/16.in
-echo "... completed 16"
-mpirun --hostfile hostfile ./sp -f ./examples/32.in
-echo "... completed 32"
-mpirun --hostfile hostfile ./sp -f ./examples/512.in
-echo "... completed 512"
-mpirun --hostfile hostfile ./sp -f ./examples/1024.in
-echo "... completed 1024"
-mpirun --hostfile hostfile ./sp -f ./examples/2048.in
-echo "... completed 2048"
-mpirun --hostfile hostfile ./sp -f ./examples/4096.in
-echo "... completed 4096"
+mpirun --hostfile hostfile -np 2 ./sp -f ./examples/1024.in
+echo "... completed p = 2"
+mpirun --hostfile hostfile -np 4 ./sp -f ./examples/1024.in
+echo "... completed p = 4"
+mpirun --hostfile hostfile -np 6 ./sp -f ./examples/1024.in
+echo "... completed p = 6"
+mpirun --hostfile hostfile -np 8 ./sp -f ./examples/1024.in
+echo "... completed p = 8"
+mpirun --hostfile hostfile -np 10 ./sp -f ./examples/1024.in
+echo "... completed p = 10"
+mpirun --hostfile hostfile -np 12 ./sp -f ./examples/1024.in
+echo "... completed p = 12"
+mpirun --hostfile hostfile -np 14 ./sp -f ./examples/1024.in
+echo "... completed p = 14"
 echo "... script complete"
