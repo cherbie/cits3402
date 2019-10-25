f8b11456087dceaf53911204babd6352bc59fe9c
Clayton <herbsca1@gmail.com>
2019-10-25 17:45:53 +0800

    collection of p=4 results

--- paths/app/test-script.sh
+++ paths/app/test-script.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 clear
-mpirun --hostfile hostfile ./sp -f ./examples/4.in
+mpirun --hostfile hostfile -np 4 ./sp -f ./examples/4.in
 echo "... completed 4"
-mpirun --hostfile hostfile ./sp -f ./examples/16.in
+mpirun --hostfile hostfile -np 4 ./sp -f ./examples/16.in
 echo "... completed 16"
-mpirun --hostfile hostfile ./sp -f ./examples/32.in
+mpirun --hostfile hostfile -np 4 ./sp -f ./examples/32.in
 echo "... completed 32"
-mpirun --hostfile hostfile ./sp -f ./examples/512.in
+mpirun --hostfile hostfile -np 4 ./sp -f ./examples/512.in
 echo "... completed 512"
-mpirun --hostfile hostfile ./sp -f ./examples/1024.in
+mpirun --hostfile hostfile -np 4 ./sp -f ./examples/1024.in
 echo "... completed 1024"
-mpirun --hostfile hostfile ./sp -f ./examples/2048.in
+mpirun --hostfile hostfile -np 4 ./sp -f ./examples/2048.in
 echo "... completed 2048"
-mpirun --hostfile hostfile ./sp -f ./examples/4096.in
+mpirun --hostfile hostfile -np 4 ./sp -f ./examples/4096.in
 echo "... completed 4096"
 echo "... script complete"
